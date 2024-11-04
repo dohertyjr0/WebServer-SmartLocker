4b0e6a320c27a0afb2f75ecca297f4826386009d
doheryjry0 <dohertyjr0@icloud.com>
2024-11-04 14:51:09 +0000

    Connected FireBase

--- WebServer/src/App.jsx
+++ WebServer/src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdAVtdU55xbal7j3a4VIRIyitYrwAl96M",
+  authDomain: "web-server-fde2a.firebaseapp.com",
+  projectId: "web-server-fde2a",
+  storageBucket: "web-server-fde2a.firebasestorage.app",
+  messagingSenderId: "781688432058",
+  appId: "1:781688432058:web:c08d5756e493536465e983",
+  measurementId: "G-HX23KEPNBY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
-  
+
 }
 
 export default App
